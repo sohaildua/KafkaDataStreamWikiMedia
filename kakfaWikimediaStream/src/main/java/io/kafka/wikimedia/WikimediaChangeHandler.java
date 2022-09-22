8b3295a2b020f597784cca0d21bdb379d0778a01
sdgamer007 <sohaildua38@gmail.com>
2022-09-22 22:31:46 +0200

    Producer with wikimedia real time data

---
+++ kakfaWikimediaStream/src/main/java/io/kafka/wikimedia/WikimediaChangeHandler.java
@@ -0,0 +1,47 @@
+package io.kafka.wikimedia;
+
+import com.launchdarkly.eventsource.EventHandler;
+import com.launchdarkly.eventsource.MessageEvent;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WikimediaChangeHandler implements EventHandler {
+
+    KafkaProducer<String,String> producer;
+    String topic;
+
+    private  final Logger log = LoggerFactory.getLogger(WikimediaChangeHandler.class.getSimpleName());
+
+    public WikimediaChangeHandler(KafkaProducer<String, String> producer, String topic) {
+        this.producer = producer;
+        this.topic = topic;
+    }
+
+    @Override
+    public void onOpen() throws Exception {
+        // no methode
+    }
+
+    @Override
+    public void onClosed() throws Exception {
+        producer.close();
+    }
+
+    @Override
+    public void onMessage(String event, MessageEvent messageEvent) throws Exception {
+        //async
+            producer.send(new ProducerRecord<>(topic, messageEvent.getData()));
+    }
+
+    @Override
+    public void onComment(String comment) throws Exception {
+            // nothing here
+    }
+
+    @Override
+    public void onError(Throwable t) {
+            log.error("Error in stream Reading"+t);
+    }
+}
---
+++ kakfaWikimediaStream/src/main/java/io/kafka/wikimedia/WikimediaChangesProducer.java
@@ -0,0 +1,45 @@
+package io.kafka.wikimedia;
+
+import com.launchdarkly.eventsource.EventHandler;
+import com.launchdarkly.eventsource.EventSource;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.net.URI;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class WikimediaChangesProducer {
+    public static void main(String[] args) throws InterruptedException {
+
+        String bootstrapServers = "127.0.0.1:9092";
+
+        //create Kafka Producer Properties
+        Properties props = new Properties();
+        props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,bootstrapServers);
+        props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // create the producer
+        KafkaProducer<String,String> producer = new KafkaProducer<String,String>(props);
+
+        String topic = "wikimedia.recentchange";
+
+        EventHandler eventHandler = new WikimediaChangeHandler(producer,topic);
+        String url = "https://stream.wikimedia.org/v2/stream/recentchange";
+
+        EventSource.Builder builder = new EventSource.Builder(eventHandler, URI.create(url));
+        EventSource eventsource = builder.build();
+
+        //start the producer
+
+        eventsource.start();
+
+
+        // we produce the data for 10 minutes
+        TimeUnit.MINUTES.sleep(1);
+
+    }
+
+}
